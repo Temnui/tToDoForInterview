5206a87d8a1454ea86399e2356f090a22aee53d7
Boboshka <temnui@gmail.com>
2021-06-26 16:24:23 +0300

    go to desktop

--- main.js
+++ main.js
@@ -4,7 +4,23 @@ let curUrl = new URL(window.location.href);
 let data = {}
 
 function getDataFromUrl() {
-    curUrl.searchParams('page') ? data.page = +curUrl.searchParams('page') : data.page = 1;
-    curUrl.searchParams('sortBy') ? data.sortBy = curUrl.searchParams('sortBy') : data.sortBy = 'username';
-    curUrl.searchParams('sortType') ? data.sortType = curUrl.searchParams('sortType') : data.sortType = 'desc';
+    //get parameters from URL if it exist, otherwise set blank data
+    if ()
+    curUrl.searchParams.get('page') ? data.page = +curUrl.searchParams.get('page') : data.page = 1;
+    curUrl.searchParams.get('sortBy') ? data.sortBy = curUrl.searchParams.get('sortBy') : data.sortBy = 'username';
+    curUrl.searchParams.get('sortType') ? data.sortType = curUrl.searchParams.get('sortType') : data.sortType = 'desc';
+    // put plank data into URL
 }
+getDataFromUrl();
+
+function insertUrlParam(key, value) {
+    data[key] = value;
+    if (history.pushState) {
+        let searchParams = new URLSearchParams(window.location.search);
+        searchParams.set(key, value);
+        let newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?' + searchParams.toString();
+        window.history.pushState({path: newurl}, '', newurl);
+    }
+}
+
+console.log('refreshed');
