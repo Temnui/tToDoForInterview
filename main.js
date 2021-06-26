32e33c78baf4ca42f7a7bf33c4d54a5a7cb1643b
temnu <temnui@gmail.com>
2021-06-26 13:52:17 +0300

    Break

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const uri = 'https://uxcandy.com/~shapoval/test-task-backend/v2/?';
 
+let curUrl = new URL(window.location.href);
+let data = {}
 
+function getDataFromUrl() {
+    curUrl.searchParams('page') ? data.page = +curUrl.searchParams('page') : data.page = 1;
+    curUrl.searchParams('sortBy') ? data.sortBy = curUrl.searchParams('sortBy') : data.sortBy = 'username';
+    curUrl.searchParams('sortType') ? data.sortType = curUrl.searchParams('sortType') : data.sortType = 'desc';
+}
